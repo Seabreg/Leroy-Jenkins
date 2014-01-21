02e33c6ab1460571c8dea4cccb47d94eeb6353d9
Captain Hooligan <jamisl@gmail.com>
2014-01-21 14:27:48 -0500

    Added gui

--- leroy-jenkins.py
+++ leroy-jenkins.py
@@ -1,14 +1,193 @@
 #!/usr/bin/env python
 
-import cmd, pycurl, os, readline, re, cStringIO
+import subprocess, pygtk, gtk, os, gobject 
+import pycurl, cStringIO, re, cmd, optparse
 
-
+nodes = ['Nodes']
 server = None
 auth = None
-node = None
-nodes = []
+cnode = None
+cnodes = []
 connected = False
 authenticated = False
+
+class Gui:
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(os.path.dirname(__file__) + '/leroy-jenkins.glade')
+        self.textview = self.builder.get_object('textview1')
+        self.window = self.builder.get_object('main_window')
+        self.textview.modify_base(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.textview.modify_text(gtk.STATE_NORMAL,gtk.gdk.color_parse('#FFFFFF'))
+        self.nodes = self.builder.get_object('nodesbox')
+        node_store = gtk.ListStore(str)
+        for node in nodes:
+            node_store.append(nodes)
+        self.nodes.set_active(0)
+        cell = gtk.CellRendererText()
+        self.nodes.set_model(node_store)
+        self.nodes.pack_start(cell, False)
+        self.nodes.add_attribute(cell, 'text', 0)
+        if self.window:
+            self.window.connect("destroy", gtk.main_quit)
+        handlers = {
+                    "command_sent"  :   self.command_sent,
+                    "destroy"       :   gtk.main_quit,
+                    "connect"       :   self.connect,
+        }
+        self.builder.connect_signals(handlers)
+
+    def connect(self, widget):
+        global nodes
+        buf = cStringIO.StringIO()
+        self.server = self.builder.get_object('entry1').get_text()
+        self.port = self.builder.get_object('entry2').get_text()
+        self.auth = self.builder.get_object('auth_check').get_active()
+        self.password = self.builder.get_object('entry3').get_text()
+        self.user = self.builder.get_object('entry4').get_text()
+        user_auth = self.user + ':' + self.password
+        self.nodes = self.builder.get_object('nodesbox')
+        c = pycurl.Curl()
+        c.setopt(c.URL, self.server + ':' + self.port + '/scriptText')
+        if self.auth is True:
+            c.setopt(c.USERPWD, user_auth)
+        c.setopt(c.POSTFIELDS, "script=for (aSlave in hudson.model.Hudson.instance.slaves) {"\
+                               "println(aSlave.name);}")
+        c.setopt(c.WRITEFUNCTION, buf.write)
+        try:
+            c.perform()
+        except pycurl.error, e:
+            errno, errstr = e
+            self.textview = self.builder.get_object('textview1')
+            self.textview.get_buffer().insert_at_cursor(errstr + '\n')
+            self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+            return False
+
+        if buf.getvalue().split('\n') == nodes:
+            return False
+        else:
+            nodes = buf.getvalue().split('\n')
+            self.nodes.clear()
+            node_store = gtk.ListStore(str)
+            cell = gtk.CellRendererText()
+            self.nodes.set_model(node_store)
+            self.nodes.pack_start(cell, False)
+            self.nodes.add_attribute(cell, 'text', 0)
+            self.nodes.append_text('Nodes')
+            self.nodes.set_active(0)
+            for node in nodes:
+                self.nodes.append_text(node)
+
+
+    def command_sent(self, widget):
+        buf = cStringIO.StringIO()
+        self.server = self.builder.get_object('entry1').get_text()
+        self.port = self.builder.get_object('entry2').get_text()
+        self.auth = self.builder.get_object('auth_check').get_active()
+        self.command = self.builder.get_object('command_entry')
+        self.local = self.builder.get_object('local_button')
+        self.remote = self.builder.get_object('remote_button')
+        self.node = self.builder.get_object('node_button')
+        self.nodes = self.builder.get_object('nodesbox')
+        status_local = self.local.get_active()
+        status_remote = self.remote.get_active()
+        status_node = self.node.get_active()
+        index = self.nodes.get_active()
+        model = self.nodes.get_model()
+        item = nodes[index - 1]
+        if status_local is True:
+            proc = subprocess.Popen(self.command.get_text(), shell=True, stdout=subprocess.PIPE)
+            gobject.io_add_watch(proc.stdout,
+                                gobject.IO_IN,
+                                self.write_output)
+            self.command.set_text('')
+        if status_remote is True:
+            if self.auth is True:
+                self.password = self.builder.get_object('entry3').get_text()
+                self.user = self.builder.get_object('entry4').get_text()
+                user_auth = self.user + ':' + self.password
+                c = pycurl.Curl()
+                c.setopt(c.URL, self.server + ':' + self.port + '/scriptText')
+                c.setopt(c.USERPWD, user_auth)
+                c.setopt(c.POSTFIELDS, 'script=println "' + self.command.get_text() + '".execute().text')
+                c.setopt(c.WRITEFUNCTION, buf.write)
+                try:
+                    c.perform()
+                    self.textview = self.builder.get_object('textview1')
+                    self.textview.get_buffer().insert_at_cursor(buf.getvalue())
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+                except pycurl.error, e:
+                    errno, errstr = e
+                    self.textview.get_buffer().insert_at_cursor(errstr + '\n')
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+            else:
+                c = pycurl.Curl()
+                c.setopt(c.URL, self.server + ':' + self.port + '/scriptText')
+                c.setopt(c.POSTFIELDS, 'script=println "' + self.command.get_text() + '".execute().text')
+                c.setopt(c.WRITEFUNCTION, buf.write)
+                try:
+                    c.perform()
+                    self.textview = self.builder.get_object('textview1')
+                    self.textview.get_buffer().insert_at_cursor(buf.getvalue())
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+                except pycurl.error, e:
+                    errno, errstr = e
+                    self.textview = self.builder.get_object('textview1')
+                    self.textview.get_buffer().insert_at_cursor(errstr + '\n')
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+        if status_node is True:
+            if self.auth is True:
+                self.password = self.builder.get_object('entry3').get_text()
+                self.user = self.builder.get_object('entry4').get_text()
+                user_auth = self.user + ':' + self.password
+                c = pycurl.Curl()
+                c.setopt(c.URL, self.server + ':' + self.port + '/computer/' + str(item) + '/scriptText')
+                c.setopt(c.USERPWD, user_auth)
+                c.setopt(c.POSTFIELDS, 'script=println "' + self.command.get_text() + '".execute().text')
+                c.setopt(c.WRITEFUNCTION, buf.write)
+                try:
+                    c.perform()
+                    self.textview = self.builder.get_object('textview1')
+                    self.textview.get_buffer().insert_at_cursor(buf.getvalue())
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+                except pycurl.error, e:
+                    errno, errstr = e
+                    self.textview.get_buffer().insert_at_cursor(errstr + '\n')
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+            else:
+                c = pycurl.Curl()
+                c.setopt(c.URL, self.server + ':' + self.port + '/computer/' + str(item) + '/scriptText')
+                c.setopt(c.POSTFIELDS, 'script=println "' + self.command.get_text() + '".execute().text')
+                c.setopt(c.WRITEFUNCTION, buf.write)
+                try:
+                    c.perform()
+                    self.textview = self.builder.get_object('textview1')
+                    self.textview.get_buffer().insert_at_cursor(buf.getvalue())
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+                except pycurl.error, e:
+                    errno, errstr = e
+                    self.textview.get_buffer().insert_at_cursor(errstr + '\n')
+                    self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+                    self.command.set_text('')
+ 
+    def write_output(self, fd, condition):
+        if condition == gobject.IO_IN:
+            char = fd.read(1)
+            self.textview = self.builder.get_object('textview1')
+            self.textview.get_buffer().insert_at_cursor(char)
+            self.textview.scroll_mark_onscreen(self.textview.get_buffer().get_insert())
+            return True
+        else:
+            return False
+
 
 class CommandShell(cmd.Cmd, object):
 
@@ -69,7 +248,7 @@ class CommandShell(cmd.Cmd, object):
 
     
     def do_nodes(self, line):
-        global nodes
+        global cnodes
         buf = cStringIO.StringIO()
         if connected is not True:
             print '[!] You must connect to server first!'
@@ -98,7 +277,7 @@ class CommandShell(cmd.Cmd, object):
                 c.setopt(c.WRITEFUNCTION, buf.write)
                 try:
                     c.perform()
-                    nodes = buf.getvalue()
+                    cnodes = buf.getvalue().split('\n')
                 except pycurl.error, e:
                     errno, errstr = e
                     print '[!] Error ' + errstr + ' filling nodes list!'
@@ -126,7 +305,7 @@ class CommandShell(cmd.Cmd, object):
                 c.setopt(c.WRITEFUNCTION, buf.write)
                 try:
                     c.perform()
-                    nodes = buf.getvalue()
+                    cnodes = buf.getvalue()
                 except pycurl.error, e:
                     errno, errstr = e
                     print '[!] Error ' + errstr + ' filling nodes list!'
@@ -139,17 +318,17 @@ class CommandShell(cmd.Cmd, object):
 
 
     def do_node(self, line):
-        global node
+        global cnode
         if connected is True:
             i = CommandShell()
             try:
-                node = line.split()[0]
+                cnode = line.split()[0]
             except Exception, e:
                 print '[!] You must specify a Node!'
                 return False
-            if node in nodes: 
-                i.prompt = "Node:" + node + ">"
-                i.intro = "\nYou are now accessing the Node " + node + ""\
+            if cnode in cnodes: 
+                i.prompt = "Node:" + cnode + ">"
+                i.intro = "\nYou are now accessing the Node " + cnode + ""\
                             "\n"
                 i.cmdloop()
             else:
@@ -168,7 +347,7 @@ class CommandShell(cmd.Cmd, object):
             print '[!] You must connect to a server first!'
         else:
             if authenticated is True:
-                if node is None:
+                if cnode is None:
                     c = pycurl.Curl()
                     c.setopt(c.URL, server + '/scriptText')
                     c.setopt(c.USERPWD, auth)
@@ -183,7 +362,7 @@ class CommandShell(cmd.Cmd, object):
                         print str(output).strip('None\n')
                 else:
                     c = pycurl.Curl()
-                    c.setopt(c.URL, server + '/computer/' + node + '/scriptText')
+                    c.setopt(c.URL, server + '/computer/' + cnode + '/scriptText')
                     c.setopt(c.USERPWD, auth)
                     c.setopt(c.POSTFIELDS, 'script=println"' + line + '".execute().text')
                     try:
@@ -193,7 +372,7 @@ class CommandShell(cmd.Cmd, object):
                         print '[!] Error ' + errstr
                         return False
             else:
-                if node is None:
+                if cnode is None:
                     c = pycurl.Curl()
                     c.setopt(c.URL, server + '/scriptText')
                     c.setopt(c.POSTFIELDS, 'script=println "' + line + '".execute().text')
@@ -207,7 +386,7 @@ class CommandShell(cmd.Cmd, object):
                         print str(output).strip('None\n')
                 else:
                     c = pycurl.Curl()
-                    c.setopt(c.URL, server + '/computer/' + node + '/scriptText')
+                    c.setopt(c.URL, server + '/computer/' + cnode + '/scriptText')
                     c.setopt(c.USERPWD, auth)
                     c.setopt(c.POSTFIELDS, 'script=println"' + line + '".execute().text')
                     try:
@@ -336,12 +515,30 @@ class CommandShell(cmd.Cmd, object):
                         "To see a list of all commands use ? or help\n"\
                         "There are some others available too!\n\n"
 
-def main():
-    try:
-        interpreter = CommandShell()
-        interpreter.cmdloop()
-    except (KeyboardInterrupt):
-        print '[-] Exiting ... '
 
-if __name__ == "__main__":
+def main():
+    parser = optparse.OptionParser(usage='%prog -g or --gui Runs the Graphical'\
+                                    'interface. %prog runs the cli', 
+                                    version='%prog Version 1.0')
+    parser.add_option('-g', '--gui', dest='gui', action='store_true', help=''\
+                        'Run the graphical interface.')
+    (options, args) = parser.parse_args()
+    gui = options.gui
+    if gui is None:
+        gui = False
+    if gui is True:
+        try:
+            interface = Gui()
+            gtk.main()
+        except (KeyboardInterrupt):
+            print '\n[-] Exiting ...'
+    if gui is False:
+        try:
+            interpreter = CommandShell()
+            interpreter.cmdloop()
+        except (KeyboardInterrupt):
+            print '\n[-] Exiting ... '
+
+
+if __name__ == '__main__':
     main()
